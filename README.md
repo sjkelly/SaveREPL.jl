a7a430eb932b712338fdd4e45b6c024d991c2cc3
Steve Kelly <kd2cca@gmail.com>
2014-08-02 02:10:40 -0400

    working saveREPL

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # SaveREPL
 
+A package to save previous inputs into the Julia REPL as scripts.
+
 [![Build Status](https://travis-ci.org/sjkelly/SaveREPL.jl.svg?branch=master)](https://travis-ci.org/sjkelly/SaveREPL.jl)
+
+## Use
+```
+julia> a = 1
+1
+
+julia> b = 2
+2
+
+julia> a + b
+3
+
+julia> p = "poop"
+"poop"
+
+julia> "skip this"
+"skip this"
+
+julia> h = "hello"
+"hello"
+
+julia> using SaveREPL
+
+julia> saveREPL("script.jl")
+	Which lines would you like to save?
+	Lines: 2, 4:7
+```
+
+This will produce a script named "script.jl" with the following contents...
+
+```
+a = 1
+b = 2
+a + b
+p = "poop"
+h = "hello"
+```
+
+Lines are numbered in reverse-cronological order. In our example, line 1 would be `julia> using SaveREPL`. Line 7 would be `julia> a = 1`.
+
--- src/SaveREPL.jl
+++ src/SaveREPL.jl
@@ -1,5 +1,73 @@
 module SaveREPL
 
-# package code goes here
+export saveREPL
+
+immutable REPLEntry
+    time::String
+    mode::String
+    command::String
+end
+
+function saveREPL(path::String)
+    println("\tWhich lines would you like to save?")
+    print("\tLines: ")
+    lines = chomp(replace(readline(), " ", ""))
+    println("Save lines $lines to $path? [Y/n]")
+    confirm = lowercase(strip(readline()))
+    if confirm == "y"
+        julia_history = history()
+        f = open(path, "w")
+        write(f, script(julia_history, lines))
+        close(f)
+    end
+end
+
+function script(histories, lines)
+    lines = reverse(split(lines, ','))
+    output = ""
+    history = reverse(histories)
+    for line in lines
+        if contains(line, ":") # found range
+            line_range = int(reverse(split(line, ':')))
+            if length(line_range) == 2
+                line_range = range(line_range[1],-1,line_range[2])
+            elseif length(line_range) == 3
+                line_range[2] = -line_range[2]
+                line_range = range(line_range...)
+            end
+            entries = history[line_range+1]
+        else
+            entries = [history[int(line)+1]]
+        end
+        for entry in entries
+            if entry.mode != "julia"
+                error("cannot output shell or help modes")
+            end
+            output *= entry.command
+        end
+    end
+    return output
+end
+
+function history()
+    julia_history = open(homedir()*"/.julia_history")
+    line = readline(julia_history)
+    histories = REPLEntry[]
+    while !eof(julia_history)
+        if contains(line, "# time: ")
+            time = replace(chomp(line), "# time: ", "")
+            mode = replace(chomp(readline(julia_history)), "# mode: ", "")
+            line = readline(julia_history)
+            command = ""
+            while beginswith(line, "\t")
+                command *= replace(line, "\t", "", 1)
+                line = readline(julia_history)
+            end
+            push!(histories, REPLEntry(time, mode, command))
+        end
+    end
+    close(julia_history)
+    return histories
+end
 
 end # module
